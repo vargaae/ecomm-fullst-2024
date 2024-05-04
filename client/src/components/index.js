90fcb1975dea80c97836341df195c5b7c843e912
vargaae <vargaae@hotmail.com>
2024-05-04 02:46:58 +0200

    003-07-SetUp  Product List Page Design

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer/Footer";
 export { default as Slider } from "./Slider/Slider";
 export { default as FeaturedProducts } from "./FeaturedProducts/FeaturedProducts";
 export { default as Card } from "./Card/Card";
+export { default as List } from "./List/List";
