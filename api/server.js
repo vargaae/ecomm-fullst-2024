84bd8ec1bc66aa66ac45bf6408e3df425a7bd5bc
vargaae <vargaae@hotmail.com>
2024-05-21 00:43:24 +0200

    prepare for deploy to render-6 setting render.yaml+update packages

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 const strapi = require("@strapi/strapi");
+
+module.exports = ({ env }) => ({
+  proxy: true,
+  url: env('HOST_URL'), // Sets the public URL of the application.
+  app: {
+    keys: env.array('APP_KEYS')
+  },
+});
+
 strapi(/* {...} */).start();
+
